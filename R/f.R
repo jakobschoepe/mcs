e1090635fc310a416c66a5872ec18443dfbeb301
jakobschoepe <jakobschoepe@protonmail.com>
2019-03-01 17:17:29 +0100

    Update f.R

--- R/f.R
+++ R/f.R
@@ -1,4 +1,22 @@
-f <- function(i, param, dim, dispstr, margins, paramMargins, n, betas, link) {
+#' @title Generating pseudo-random data for Monte Carlo simulations using a normal copula.
+#' @description \code{dg} generates pseudo-random data for Monte Carlo simulations 
+#' @param i 
+#' @param param A numeric vector specifying the dispersion matrix (see \code{copula} package).
+#' @param dim An integer specifying the dimension of the copula (see \code{copula} package).
+#' @param dispstr A character string specifying the dispersion structure (see \code{copula} package): autoregressive of order 1 (ar1), exchangeable (ex), Toeplitz (toep), and unstructured (un).
+#' @param margins A character vector specifying the marginal distributions (see \code{copula} package).
+#' @param paramMargins An object of class \code{list} giving the parameter values of the marginal distributions (see \code{copula} package).
+#' @param n An integer specifying the number of observations to be generated.
+#' @param f A model formula.
+#' @param betas A numeric vector specifying the true model parameters/estimands.
+#' @param link A character string specifying the link function for model fitting ("logit" or "log").
+#' @details \code{dg} was built as part of the design of a Monte Carlo simulation, and serves a special-purpose only.
+#' @return A list containing the following elements:
+#' \item{seed}{An integer vector containing the random number generator state.}
+#' \item{data}{A data table containing the pseudo-random generated data.}
+#' @references Yan J (2007) Enjoy the joy of copulas: With a package copula. J Stat Softw, 21:1-21
+
+dg <- function(i, param, dim, dispstr, margins, paramMargins, n, f, betas, link) {
   # Check passed arguments to smoothly run subsequent commands
   if (!is.vector(x = param)) {
     stop("\"param\" must be a real vector")
@@ -67,8 +85,8 @@ f <- function(i, param, dim, dispstr, margins, paramMargins, n, betas, link) {
     # Generate random variables from marginal distributions
     simdata <- data.table::as.data.table(copula::rMvdc(n = n, mvdc = tmp2))
   
-    # Predefine linear combination
-    b <- model.matrix(~ ., data = simdata) %*% betas
+    # Predefine linear combinations
+    b <- model.matrix(f, data = simdata) %*% betas
   
     #  
     if (link == "logit") {
