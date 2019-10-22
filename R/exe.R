48a9066429a2ebbac68fad5cc3d282b63a8ba78d
jakobschoepe <jakobschoepe@protonmail.com>
2019-10-22 10:46:40 +0200

    Update exe.R

--- R/exe.R
+++ R/exe.R
@@ -17,15 +17,10 @@ m[upper.tri(m)] <- t(m)[upper.tri(m)]
 diag(m) <- 1
 param <- t(m)[upper.tri(m)]
 
-
-
-
-
-
 param <- c(0.3, -0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -0.6, -0.3, 0, 0.8, 0.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, -0.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.3, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0.5, 0, 0, 0)
 margins <- c("norm", "binom", "binom", "beta", "norm", "binom", "gamma", "norm", "binom", "binom", "gamma", "pois", "pois", "norm", "beta", "norm")
 paramMargins <- list(list(mean = 15, sd = 3), list(size = 1, prob = 0.3), list(size = 1, prob = 0.7), list(shape1 = 20, shape2 = 2), list(mean = 80, sd = 5), list(size = 1, prob = 0.1), list(shape = 2, scale = 2), list(mean = 60, sd = 10), list(size = 1, prob = 0.2), list(size = 1, prob = 0.5), list(shape = 1, scale = 2), list(lambda = 4), list(lambda = 1), list(mean = 150, sd = 15), list(shape1 = 15, shape2 = 1), list(mean = 40, sd = 3))
-f <- X1 + X4 + X6 + X8 + X9 + X10 + X11 + X13
+f <- ~ X1 + X4 + X6 + X8 + X9 + X10 + X11 + X13
 
 thetas <- c(0.0370, -0.0130, -0.3600, 0.0220, 0.6930, 0.2000, -0.0021, 0.0400)
 
