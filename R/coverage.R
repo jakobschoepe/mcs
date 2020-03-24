8f46da818de7cbcfd770eb15eb748deb38bc6a7d
jakobschoepe <jakobschoepe@protonmail.com>
2020-03-24 13:54:23 +0100

    Update coverage.R

--- R/coverage.R
+++ R/coverage.R
@@ -1,13 +1,20 @@
-#' @title Assess the coverage of estimated confidence intervals
-#' @description 
-#' @usage coverage(x, varnames, true)
-#' @param x A numeric matrix containing the confidence interval limits.
-#' @param varnames A character vector giving the names i
-#' @param true A numeric vector giving the true value(s).
-#' @details
-#' @return
+#' @title Estimating the coverage probability of a confidence interval estimator of interest
+#' @description \code{coverage()} estimates the coverage probability of a confidence interval estimator of interest in the context of a Monte Carlo simulation.
+#' @usage coverage(x, true)
+#' @param x A numeric matrix containing the estimated limits of the confidence interval estimator of interest.
+#' @param true A numeric vector giving the estimand.
+#' @details The coverage probability of a confidence interval estimator of interest is derived from
+#' \begin{equation*}
+#' \frac{1}{n_{sim}}\displaystyle\sum_{i=1}^{n_{sim}} 1(\hat{\theta}_{low,i}\leq\theta\leq\hat{\theta}_{upp,i})
+#' \end{equation*}
+#' where $n_{sim}$ is the number of repetitions of the Monte Carlo simulation, $\hat{\theta}_{low,i}$ and $\hat{\theta}_{upp,i}$ are the estimated limits of the confidence interval estimator of interest of the $i^{th}$ repetition, and $\theta$ is the estimand. The corresponding Monte Carlo standard error is derived from
+#' \begin{equation*}
+#' \sqrt{\frac{\widehat{Cover.} \times (1 - \widehat{Cover.})}{n_{sim}}}
+#' \end{equation*}
+#' where $\widehat{Cover.}$ is the estimated coverage probability of the confidence interval estimator of interest, and $n_{sim}$ is the number of repetitions of the Monte Carlo simulation.
+#' @return A numeric matrix giving the estimated coverage probability of the confidence interval estimator of interest, and the corresponding Monte Carlo standard error.
 #' @references Morris TP, White IR, Crowther MJ (2019) Using simulation studies to evaluate statistical methods. Stat Med 38:2074-2102
-#' @notes Please note that \code{coverage} was built as part of the design of a Monte Carlo simulation, and therefore serves a special-purpose only.
+#' @notes Please note that \code{coverage()} was built as part of the design of a Monte Carlo simulation, and therefore serves a special-purpose only.
 #' @author Jakob Schöpe
 
 coverage <- function(x, true) {
