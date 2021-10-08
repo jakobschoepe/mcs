92a38180840ab09c58f7e29878b4f2ca03afdf29
Jakob Schöpe <jakobschoepe@protonmail.com>
2021-10-08 12:14:00 +0200

    Update test-bias.R

--- tests/testthat/test-bias.R
+++ tests/testthat/test-bias.R
@@ -1,3 +1,3 @@
 test_that(desc = "Bias and its Monte Carlo standard error are equal to 2 and 0.70710678",
-          code = {expect_equal(object = bias(x = matrix(1:5, nrow = 5), true = 1),
-                                         expected = array(c(2, 0.70710678), dim = c(1,2), dimnames = list(NULL, c("Bias", "SE"))))})
+          code = {expect_equal(object = bias(x = matrix(data = 1:5, nrow = 5), true = 1),
+                               expected = array(data = c(2, 0.70710678), dim = c(1,2), dimnames = list(NULL, c("Bias", "SE"))))})
