7c82a3f78736819fcf839df382e55ab5a7476095
Jakob Schöpe <jakobschoepe@protonmail.com>
2021-10-08 12:11:40 +0200

    Create test-coverage.R

---
+++ tests/testthat/test-coverage.R
@@ -0,0 +1,3 @@
+test_that(desc = "Coverage and its Monte Carlo standard error are equal to 0.8 and 0.17888543",
+          code = {expect_equal(object = coverage(x = array(1:10, dim = c(5,2), dimnames = list(NULL, c("x.cil", "x.ciu"))), 4),
+                               expected = array(c(0.8, 0.17888543), dim = c(1,2), dimnames = list("x", c("Coverage", "SE"))))})
